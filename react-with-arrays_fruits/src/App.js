da8d7ba74eada9ed128f8142f6b3b86d08bd13a1
Smail Bensaad <amsmailbensaad@gmail.com>
2023-08-01 15:48:41 +0200

    newbranch2

--- react-with-arrays_fruits/src/App.js
+++ react-with-arrays_fruits/src/App.js
@@ -1,11 +1,20 @@
-import Card from "./components/Card";
+import React from 'react';
+import Card from './components/Card';
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: '🍌 Banana', color: 'yellow' },
+    { id: 2, name: '🍎 Apple', color: 'red' },
+    { id: 3, name: '🍊 Orange', color: 'orange' },
+    { id: 4, name: '🍇 Grapes', color: 'purple' },
+    { id: 5, name: '🍍 Pineapple', color: 'brown' },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} />
+      ))}
     </div>
   );
 }
