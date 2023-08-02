d8a21a045734086940e9a84137a1c9eb27cf7a43
Smail Bensaad <amsmailbensaad@gmail.com>
2023-08-02 17:03:42 +0200

    initialcommit

--- react-state-2_table-reservation/src/App.js
+++ react-state-2_table-reservation/src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  // Move the state to this component
+  const [people, setPeople] = useState(0);
+
+  // Create handlers for increment and decrement
+  const handleIncrement = () => {
+    setPeople(people + 1);
+  };
+
+  const handleDecrement = () => {
+    if (people > 0) {
+      setPeople(people - 1);
+    }
+  };
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      {/* Pass the handlers as props to the Counter component */}
+      <Counter onIncrement={handleIncrement} onDeccrement={handleDecrement} />
+      {/* Dynamically render the count of people */}
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
+
